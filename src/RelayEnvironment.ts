451011766ffaa3fc0b1d075311672e020087a631
MitsukiIwamura <boc.fsfb@gmail.com>
2022-12-03 19:38:02 +0900

    relayEnvironmentの型指定

--- src/RelayEnvironment.ts
+++ src/RelayEnvironment.ts
@@ -1,15 +1,18 @@
 // your-app-name/src/RelayEnvironment.js
-import { Environment, Network, RecordSource, Store } from "relay-runtime";
+import {
+  Environment,
+  FetchFunction,
+  Network,
+  RecordSource,
+  Store,
+} from "relay-runtime";
 import fetchGraphQL from "./fetchGraphQL";
 
 // Relay passes a "params" object with the query name and text. So we define a helper function
 // to call our fetchGraphQL utility with params.text.
-async function fetchRelay(params: { name: any; text: any }, variables: any) {
-  console.log(
-    `fetching query ${params.name} with ${JSON.stringify(variables)}`
-  );
-  return fetchGraphQL(params.text, variables);
-}
+const fetchRelay: FetchFunction = async (query, variables) => {
+  return fetchGraphQL(query, variables);
+};
 
 // Export a singleton instance of Relay Environment configured with our network function:
 export default new Environment({
--- src/fetchGraphQL.ts
+++ src/fetchGraphQL.ts
@@ -1,14 +1,13 @@
-import { Variables } from "relay-runtime";
+import { RequestParameters, Variables } from "relay-runtime";
 
-const fetchGraphQL = async (query: string, variables: Variables) => {
-  // Fetch data from GitHub's GraphQL API:
+const fetchGraphQL = async (query: RequestParameters, variables: Variables) => {
   const response = await fetch("https://graphql-pokemon2.vercel.app", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      query,
+      query: query.text,
       variables,
     }),
   });
